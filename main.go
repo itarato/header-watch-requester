c0faab48c9f83393725eae4a22964b762d43a54c
Peter Arato <it.arato@gmail.com>
2015-10-08 12:07:41 +0800

    Add crawler input parser.

--- main.go
+++ main.go
@@ -1,22 +1,84 @@
+// @TODO
+// - make it secure (maybe POST or JWT)
+
 package main
 
 import (
+	"encoding/json"
+	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"time"
 )
 
+var ()
+
+// ServerHandler handles incoming requests.
 type ServerHandler struct {
 }
 
+// CrawlingLocation keeps the location info in incoming JSON payload.
+type CrawlingLocation struct {
+	URL string `json:"url"`
+}
+
+// CrawlingInfoJSON is the format of the incoming JSON.
+type CrawlingInfoJSON struct {
+	Locations []CrawlingLocation `json:"locations"`
+}
+
+// CrawlingLocationResult keeps records of location crawl results.
+type CrawlingLocationResult struct {
+	URL     string            `json:"url"`
+	Headers map[string]string `json:"headers"`
+}
+
+// CrawlingResponseJSON is the returned object.
+type CrawlingResponseJSON struct {
+	Locations []CrawlingLocationResult `json:"locations"`
+}
+
 func (sh *ServerHandler) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
-	log.Println("called http.ServeHTTP")
+	log.Println("incoming request")
+
+	payload := &CrawlingInfoJSON{}
+	b, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		rw.WriteHeader(400)
+		io.WriteString(rw, "Unable to read payload.")
+		return
+	}
+
+	if err = json.Unmarshal(b, payload); err != nil {
+		rw.WriteHeader(400)
+		io.WriteString(rw, "Unable to parse JSON. "+err.Error())
+		return
+	}
+
+	responseJSON := &CrawlingResponseJSON{}
+	for _, location := range payload.Locations {
+		log.Println(location.URL)
+		locationInfo := CrawlingLocationResult{URL: location.URL, Headers: make(map[string]string)}
+		responseJSON.Locations = append(responseJSON.Locations, locationInfo)
+	}
+
+	jsonOut, err := json.Marshal(responseJSON)
+	if err != nil {
+		rw.WriteHeader(400)
+		io.WriteString(rw, "Unable to generate JSON. "+err.Error())
+		return
+	}
+
+	rw.WriteHeader(200)
+	rw.Write(jsonOut)
 }
 
 func main() {
 	log.Println("crawler has been started")
 
 	server := &http.Server{
+		// @TODO make it a configuration
 		Addr:           "localhost:8080",
 		Handler:        &ServerHandler{},
 		ReadTimeout:    time.Second * 60,
@@ -24,13 +86,16 @@ func main() {
 		MaxHeaderBytes: 0,
 	}
 
-	log.Println(server)
+	log.Println("server has been initialized")
 
 	err := server.ListenAndServe()
 	if err != nil {
 		log.Panicln(err)
 	}
 
+	log.Println("listen loop has been started")
+
+	// @todo figure out if this is effective
 	for {
 	}
 }
